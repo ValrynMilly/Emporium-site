65ac43983e89bc146c2f77e88e088facf635a7c5
Emiljano <ekurbiba@gmail.com>
2024-07-13 02:15:21 +0100

    Many changes
    I added a ESRGAN to the website for AI Upscalar functionality.
    
    A modal video player (youtube video) pops up now for the Face detector.

--- app.py
+++ app.py
@@ -4,12 +4,51 @@ import os
 import CVS as htm
 from flask import Flask
 from flask import render_template, Response, request, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
+from werkzeug.utils import secure_filename
+from wtforms.validators import InputRequired
+import urllib.request
+import glob
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secretkey'
+app.config['UPLOAD_FOLDER'] = 'ESRGAN\ESRGAN\LR'
 
+files = glob.glob('ESRGAN/LR/')
 
-@app.route("/")
+class UploadFileForm(FlaskForm):
+    file = FileField("File")
+    submit = SubmitField("Upload File")
+
+def delete_files_in_directory(directory_path):
+   try:
+     files = os.listdir(directory_path)
+     for file in files:
+       file_path = os.path.join(directory_path, file)
+       if os.path.isfile(file_path):
+         os.remove(file_path)
+     print("All files deleted successfully.")
+   except OSError:
+     print("Error occurred while deleting files.")
+
+directory_path = 'ESRGAN\ESRGAN\LR'
+directory_path2 = 'ESRGAN\ESRGAN/results'
+
+@app.route("/", methods=['GET', "POST"])
+@app.route("/home", methods=['GET', "POST"])
 def landing():
-    return render_template('landing.html')
+    form = UploadFileForm()
+    if form.validate_on_submit():
+        file = form.file.data
+        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename)))
+        os.chdir('ESRGAN\ESRGAN/')
+        os.system('python test.py')
+        os.chdir('..')
+        os.chdir('..')
+        delete_files_in_directory(directory_path)
+        delete_files_in_directory(directory_path2)
+    return render_template('landing.html', form=form)
 
 
 @app.route('/video_feed', methods=['POST'])
