b7015c7a216917e707a3f00ea1428e6f6efa9323
Emiljano <ekurbiba@gmail.com>
2024-07-14 23:56:36 +0100

    Updated
    Upscalar working and does not save files

--- app.py
+++ app.py
@@ -3,17 +3,17 @@ import time
 import os
 import CVS as htm
 from flask import Flask
-from flask import render_template, Response, request, redirect, url_for
+from flask import render_template, Response, request, redirect, url_for, send_from_directory
 from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField
 from werkzeug.utils import secure_filename
 from wtforms.validators import InputRequired
-import urllib.request
 import glob
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secretkey'
+app.config['SECRET_KEY'] = 'secretkey' #<------- HIDE THIS IN ENV VARIABLES
 app.config['UPLOAD_FOLDER'] = 'ESRGAN\ESRGAN\LR'
+app.config['DOWNLOAD_FOLDER'] = 'ESRGAN\ESRGAN/results'
 
 files = glob.glob('ESRGAN/LR/')
 
@@ -32,8 +32,15 @@ def delete_files_in_directory(directory_path):
    except OSError:
      print("Error occurred while deleting files.")
 
-directory_path = 'ESRGAN\ESRGAN\LR'
+directory_path = 'ESRGAN\ESRGAN\LR'  
 directory_path2 = 'ESRGAN\ESRGAN/results'
+cwd = os.getcwd()
+
+
+def download_files_in_directory(directory_path2):
+     files_in = os.listdir(directory_path2)[0]
+     send_from_directory(directory_path2, files_in, as_attachment=True)
+
 
 @app.route("/", methods=['GET', "POST"])
 @app.route("/home", methods=['GET', "POST"])
@@ -46,6 +53,7 @@ def landing():
         os.system('python test.py')
         os.chdir('..')
         os.chdir('..')
+        download_files_in_directory(directory_path2)
         delete_files_in_directory(directory_path)
         delete_files_in_directory(directory_path2)
     return render_template('landing.html', form=form)
